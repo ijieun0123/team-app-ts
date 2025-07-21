bc41326f59c784df95cb2c6fabd96e02521f6d98
이지은 <tori@ryanlab.kr>
2025-07-21 10:11:38 +0900

    헤더 스크롤 이벤트 변경 ( show / hide, white_bg )

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,44 @@ import React, { useRef, useState, useEffect } from "react";
 const Header: React.FC = () => {
     const location = useLocation();
     const headerRef = useRef<HTMLElement | null>(null);
-    const [isDark, setIsDark] = useState(false);
+    const [isWhiteBg, setIsWhiteBg] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [showHeader, setShowHeader] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
 
+    // 헤더 스크롤 이벤트 ( show, hide )
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            // 스크롤이 최상단이면 헤더를 보이게
+            if (currentScrollY === 0) {
+                setShowHeader(true);
+            }
+            // 스크롤 할 경우 헤더를 안 보이게
+            else {
+                setShowHeader(false);
+            }
+
+            setLastScrollY(currentScrollY);
+        };
+
+        const handleMouseMove = (e: MouseEvent) => {
+            if (e.clientY < 50) {
+                setShowHeader(true); // 커서가 화면 상단 근처면 다시 헤더 보이기
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, [lastScrollY]);
+
+    // 햄버거 메뉴 클릭 이벤트
     const handleHamburgerClick = () => {
         if (headerRef.current) {
             headerRef.current.classList.add("menu_open");
@@ -15,6 +50,7 @@ const Header: React.FC = () => {
         setIsMenuOpen(true);
     };
 
+    // 클로즈 버튼 클릭 이벤트
     const handleCloseBtnClick = () => {
         if (headerRef.current) {
             headerRef.current.classList.remove("menu_open");
@@ -22,38 +58,41 @@ const Header: React.FC = () => {
         setIsMenuOpen(false);
     };
 
+    // 헤더 스크롤 이벤트 ( white_bg )
     useEffect(() => {
         const handleScroll = () => {
-            if (location.pathname != "/") {
-                setIsDark(true);
+            if (location.pathname !== "/") {
+                setIsWhiteBg(true);
                 return;
             }
 
-            const hubSection = document.querySelector(
-                ".hub"
-            ) as HTMLElement | null;
             const scrollY = window.scrollY;
-            const hubSectionTop = hubSection?.offsetTop || 0;
 
-            if (scrollY >= hubSectionTop - 10) {
-                setIsDark(true);
+            if (scrollY === 0) {
+                setIsWhiteBg(false);
             } else {
-                setIsDark(false);
+                setIsWhiteBg(true);
             }
         };
 
+        handleScroll();
         addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    }, [location.pathname]);
 
     return (
-        <header ref={headerRef} className={isDark ? "black" : ""}>
+        <header
+            ref={headerRef}
+            className={`${showHeader ? "show" : "hide"} ${
+                isWhiteBg ? "white_bg" : ""
+            }`}
+        >
             <div className="container">
                 <Link to="/" className="logo">
                     <img
                         className="header_logo"
                         src={
-                            isDark
+                            isWhiteBg
                                 ? "/team-app-ts/img/black_logo.svg"
                                 : "/team-app-ts/img/white_logo.png"
                         }
@@ -69,7 +108,7 @@ const Header: React.FC = () => {
                 >
                     <img
                         src={
-                            isDark
+                            isWhiteBg
                                 ? "/team-app-ts/img/black_menu.svg"
                                 : "/team-app-ts/img/white_menu.svg"
                         }
