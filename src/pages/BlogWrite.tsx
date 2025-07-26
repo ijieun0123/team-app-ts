684a77755d2a4fa1f4171fde24f1a26f698eb3f2
이지은 <tori@ryanlab.kr>
2025-07-26 15:17:35 +0900

    BlogWrite.scss -> FormPage.scss 로 이름 변경

--- src/pages/BlogWrite.tsx
+++ src/pages/BlogWrite.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, type FC } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "../styles/BlogWrite.scss";
+import "../styles/FormPage.scss";
 import InputTextarea from "../components/InputTextarea";
 import Button from "../components/Button";
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState, type FC } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "../styles/BlogWrite.scss";
+import "../styles/FormPage.scss";
 import InputTextarea from "../components/InputTextarea";
 import Button from "../components/Button";
 
