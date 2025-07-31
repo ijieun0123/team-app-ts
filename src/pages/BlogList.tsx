ee877104cd0089c1e09652cd56ce7e6461e341a2
이지은 <tori@ryanlab.kr>
2025-07-31 12:36:31 +0900

    API_BASE_URL 출력 테스트

--- src/pages/BlogList.tsx
+++ src/pages/BlogList.tsx
@@ -49,7 +49,7 @@ const BlogList: FC = () => {
 
     useEffect(() => {
         fetchBlogData(0);
-        console.log("API_BASE_URL: " + import.meta.env.VITE_API_BASE_URL);
+        console.log("API_BASE_URL: " + API_BASE_URL);
     }, []);
 
     return (
